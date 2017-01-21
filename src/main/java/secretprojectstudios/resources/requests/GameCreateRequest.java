56f266ee7eff0eebd3a0ac649b3ae9420943a60d
Jonathan Peyper <jonathan.peyper@ioof.com.au>
2017-01-21 18:39:37 +1100

    Fix null pointer

--- src/main/java/secretprojectstudios/resources/requests/GameCreateRequest.java
+++ src/main/java/secretprojectstudios/resources/requests/GameCreateRequest.java
@@ -1,9 +1,13 @@
 package secretprojectstudios.resources.requests;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
+@JsonDeserialize
 public class GameCreateRequest {
     private String playerName;
 
-    public GameCreateRequest(String playerName) {
+    public GameCreateRequest(@JsonProperty("playerName") String playerName) {
         this.playerName = playerName;
     }
 
