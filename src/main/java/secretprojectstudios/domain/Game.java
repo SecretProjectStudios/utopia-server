d0de4e0ac6144ce9ecbe724286d5e035d8bb01ac
alex <thatalextaylor@yahoo.com.au>
2017-01-21 13:51:47 +1100

    Added player

--- src/main/java/secretprojectstudios/domain/Game.java
+++ src/main/java/secretprojectstudios/domain/Game.java
@@ -1,10 +1,16 @@
 package secretprojectstudios.domain;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.jongo.marshall.jackson.oid.MongoId;
 import org.jongo.marshall.jackson.oid.MongoObjectId;
+
+import java.util.Set;
 
 public class Game {
     @MongoId // auto
     @MongoObjectId
     private String id;
+
+    @JsonProperty
+    private Set<Player> players;
 }
---
+++ src/main/java/secretprojectstudios/domain/Player.java
@@ -0,0 +1,34 @@
+package secretprojectstudios.domain;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.jongo.marshall.jackson.oid.MongoId;
+import org.jongo.marshall.jackson.oid.MongoObjectId;
+
+import java.util.UUID;
+
+public class Player {
+    private String id;
+
+    private String name;
+
+    @JsonCreator
+    protected Player(@MongoId @MongoObjectId String id, @JsonProperty("name") String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Player(String name) {
+        this(null, name);
+    }
+
+    @JsonProperty("_id")
+    public String getId() {
+        return id;
+    }
+
+    @JsonProperty
+    public String getName() {
+        return name;
+    }
+}
