0b7f8c74ea9379999b52770ef149f5b5fb933c07
Michael Peyper <michael.peyper@ioof.com.au>
2017-01-21 11:42:35 +1100

    added dropwizard

---
+++ src/main/java/secretprojectstudios/Run.java
@@ -0,0 +1,8 @@
+package secretprojectstudios;
+
+public class Run {
+
+    public static void main(String[] args) throws Exception {
+        UtopiaServer.main(new String[] { "server", "conf/utopia.yml" });
+    }
+}
--- src/main/java/secretprojectstudios/UtopiaServer.java
+++ src/main/java/secretprojectstudios/UtopiaServer.java
@@ -1,8 +1,27 @@
 package secretprojectstudios;
 
-public class UtopiaServer {
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+import secretprojectstudios.setup.configuration.UtopiaConfiguration;
 
-    public static void main(String[] args) {
+public class UtopiaServer extends Application<UtopiaConfiguration> {
+
+    public static void main(String[] args) throws Exception {
+        new UtopiaServer().run(args);
+    }
+
+    @Override
+    public String getName() {
+        return "utopia-server";
+    }
+
+    @Override
+    public void initialize(Bootstrap<UtopiaConfiguration> bootstrap) {
+
+    }
+
+    public void run(UtopiaConfiguration utopiaConfiguration, Environment environment) throws Exception {
 
     }
 }
